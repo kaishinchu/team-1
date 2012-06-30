cf5e8079ca406beaa05ed98ca514f5724cb3ba78
thoronas <fsquaredesign@gmail.com>
2012-06-30 16:09:29 -0700

    Added custom search page

--- page-custom-search.php
+++ page-custom-search.php
@@ -22,15 +22,34 @@ get_header(); ?>
 			<div id="content" class="eight columns" role="main">
 				<div class="post-box">
 					<?php 
-						$searchargs = array (
-						
+						$searchArgs = array (
+							'post_type' => 'recipe',
+							'posts_per_page' => -1, 
 						);
-					?>
-					<?php the_content(); ?>
-					<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
-					<?php 	endwhile; // End the loop 
+					    $wp_query = new WP_Query($searchArgs);		
+						if($wp_query->have_posts()){
+							while ($wp_query->have_posts()) : $wp_query->the_post(); 
+						?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+							<div class="three columns">
+								<?php the_post_thumbnail('thumbnail'); ?>
+							</div>
+							<div class="nine columns">
+								<header>
+									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									<h3><?php echo get_the_term_list( $post->ID, 'style', 'Style: ', ', ', '' ); ?> </h3>
+									<?php echo get_the_term_list( $post->ID, 'ingredients', 'Ingredients: ', ', ', '' ); ?> 
+								</header>
+								<div class="entry-content">
+									<?php the_excerpt(); ?>
+								</div>
+							</div>
+						</article>	
+						<?php 
+							endwhile; 
 							wp_reset_query(); 
-					?>			
+						} // end if
+					?>		
 				</div>
 	
 			</div><!-- End Content row -->
